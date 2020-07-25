94bd6be202e4f1a0dd6bf12180456e0cf52659fa
belen-dominguez <belen.dominguez.sigler@outlook.com>
2020-07-25 16:44:34 -0300

    Updates

---
+++ precache-manifest.b12c022a57fcc70ef20cb7eef84ecff4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c6f624597468599bc8bd4b5fe5ccb0c",
+    "url": "/React_TP-todoList/index.html"
+  },
+  {
+    "revision": "7058bb92aef40fba0229",
+    "url": "/React_TP-todoList/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a6ab947a50f60b0e2bea",
+    "url": "/React_TP-todoList/static/js/2.59c09e3d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_TP-todoList/static/js/2.59c09e3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7058bb92aef40fba0229",
+    "url": "/React_TP-todoList/static/js/main.a6927144.chunk.js"
+  },
+  {
+    "revision": "670d35767b15c04267d0",
+    "url": "/React_TP-todoList/static/js/runtime-main.0680f50f.js"
+  }
+]);
